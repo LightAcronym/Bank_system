2c395d7a908c0dffeceab0701911cfd9f4edeae4
LightAcronym <orgewe@gmail.com>
2024-02-29 21:28:08 +0200

    first python program

---
+++ git_ex_3.py
@@ -0,0 +1,22 @@
+class BankSystem:
+    def __init__(self):
+        self.accounts = {}
+
+    def account_creation(self):
+        return None
+
+    def balance_inquiry(self):
+        return None
+
+    def deposit(self):
+        return None
+
+    def withdrawal(self):
+        return None
+
+    def account_summary(self):
+        return None
+
+
+if __name__ == '__main__':
+    print('PyCharm')
